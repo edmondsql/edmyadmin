82df9e4e15f5a0e6fb45a7d02a09009c34617d0e
Edmond <edmondsql@gmail.com>
2026-06-10 10:11:42 +0300

    3.27
    -change encode and decode functions

--- sql.php
+++ sql.php
@@ -5,7 +5,7 @@ session_name('SQL');
 session_start();
 $bg=2;
 $step=20;
-$version="3.26";
+$version="3.27";
 $bbs=['False','True'];
 $deny=['mysql','information_schema','performance_schema','sys'];
 class DBT {
@@ -104,7 +104,7 @@ class DBT {
 	}
 }
 class ED {
-	public $con,$path,$sg,$u_db,$fieldtype,$sqlda;
+	public $con,$path,$sg,$u_db,$fieldtype,$sqlda,$cif='@#$';
 	public function __construct(){
 	$pi=(isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : @getenv('PATH_INFO'));
 	$this->sg=preg_split('!/!',$pi,-1,PREG_SPLIT_NO_EMPTY);
@@ -161,40 +161,19 @@ class ED {
 		}
 		header('Location: '.$this->path.$way);exit;
 	}
-	public function enco($str){
-		$salt=$_SERVER['HTTP_USER_AGENT'];
-		$count=strlen($str);
-		$str=(string)$str;
-		$kount=strlen($salt);
-		$x=0;$y=0;
-		$eStr="";
-		while($x < $count){
-			$char=ord($str[$x]);
-			$keyS=is_numeric($salt[$y]) ? $salt[$y] : ord($salt[$y]);
-			$encS=$char + $keyS;
-			$eStr.=chr($encS);
-			++$x;++$y;
-			if($y==$kount) $y=0;
-		}
-		return base64_encode(base64_encode($eStr));
+	public function enco($str) {
+		$salt = random_bytes(16);
+		$key = hash('sha256', $this->cif.$salt, true);
+		$encrypted = $str ^ str_repeat($key, ceil(strlen($str) / strlen($key)));
+		return base64_encode($salt . $encrypted);
 	}
-	public function deco($str){
-		$salt=$_SERVER['HTTP_USER_AGENT'];
-		$str=base64_decode(base64_decode($str));
-		$count=strlen($str);
-		$str=(string)$str;
-		$kount=strlen($salt);
-		$x=0;$y=0;
-		$eStr="";
-		while($x < $count){
-			$char=ord($str[$x]);
-			$keyS=is_numeric($salt[$y]) ? $salt[$y] : ord($salt[$y]);
-			$decS=$char - $keyS;
-			$eStr.=chr($decS);
-			++$x;++$y;
-			if($y==$kount) $y=0;
-		}
-		return $eStr;
+	public function deco($str) {
+		$data = base64_decode($str, true);
+		if ($data === false || strlen($data) < 16) return false;
+		$salt = substr($data, 0, 16);
+		$cipher = substr($data, 16);
+		$key = hash('sha256', $this->cif.$salt, true);
+		return $cipher ^ str_repeat($key, ceil(strlen($cipher) / strlen($key)));
 	}
 	public function priv($pr,$redir=NULL){
 		$usr=$_SESSION['user'];
